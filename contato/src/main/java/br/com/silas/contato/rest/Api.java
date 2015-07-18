aedd59b12b9e578d71f2fdc52c1fc9cf11f44402
jose <silas@silas>
2015-07-18 14:49:22 -0400

    versao inicial do projeto.

---
+++ contato/src/main/java/br/com/silas/contato/rest/Api.java
@@ -0,0 +1,9 @@
+package br.com.silas.contato.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class Api extends Application {
+
+}
